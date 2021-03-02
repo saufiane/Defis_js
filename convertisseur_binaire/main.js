3f6a3f423d5b0e2c68810b6c122fe73fa31b10c9
Saufiane CHERIEF <72612551+saufiane@users.noreply.github.com>
2021-03-02 11:14:52 +0100

    Update main.js

--- convertisseur_binaire/main.js
+++ convertisseur_binaire/main.js
@@ -1,14 +1,30 @@
 const convert = document.getElementById("convert");
 
 convert.addEventListener("click", function(){
-    let inputCode= document.getElementById("msgToCode").value;
-    let outputCrypted= document.getElementById("crypted");
-     outputCrypted.value="";
-           for (var i= 0; i < inputCode.length; i++) {
-        outputCrypted.value += inputCode[i].charCodeAt(0).toString(2) + " ";
-        console.log(outputCrypted.value);
-       
-}
+    let input= document.getElementById("msgToCode").value;
+    // click convert ecoute
+    // recup dans txt
+    // moulinette txt en bin string to binary
+    // faire test regu en console console.log();
+    // afficha dans id="crypted"
+
+    
+    
+    // recup les données entrée dans le champ
+    let output= document.getElementById("crypted");
+    console.log(input);
+
+     output.value="";
+    //  convertir en binaire
+           for (var i= 0; i < input.length; i++) {
+        output.value += input[i].charCodeAt(0).toString(2) + " ";
+            
+}   
+        console.log(i);
+        console.log(output.value);
+        //afficher le result 
+        output.innerHTML = output.value;
+
 });
   
 //charCodeAt = sa renvoie le code ASCII;
